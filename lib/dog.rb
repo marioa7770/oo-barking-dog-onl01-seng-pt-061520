6d839c16ccb6f94693d3654dfb9c4d8db59883b1
mario acevedo <jun_acevedo415@yahoo.com>
2020-07-03 22:21:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,21 +1,17 @@
 class Dog
- 
   def name=(dogs_name)
-    @this_dogs_name = dogs_name
-  end
- 
+    @name = dogs_name
+  end 
+
   def name
-    @this_dogs_name
-  end
-end
- 
-lassie = Dog.new
-lassie.name = "Lassie"
-lassie.bark = "Bark"
-puts lassie.name
+    @name
+  end 
+  
+end 
 
 class Dog
-  def Bark
-    puts "woof"!
-  end
-end
+  def bark
+    puts "woof!"
+  end 
+  
+end 
